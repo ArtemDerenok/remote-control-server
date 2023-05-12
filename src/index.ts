08600b4a8f5c91f2ddd033e21af8a95325bbadba
ArtemDerenok <derenokartem95@gmail.com>
2023-05-12 21:52:23 +0300

    refactor: improve wss

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,35 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 import mouseControler from './controlers/mouseControler';
 import drawControler from './controlers/drawingControler';
 
+declare interface MyWS extends WebSocket {
+  isAlive?: boolean;
+}
+
+function heartbeat() {
+  this.isAlive = true;
+}
+
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', (ws) => {
+wss.on('connection', (ws: MyWS, req) => {
+  ws.isAlive = true;
+
+  console.log(`${req.socket.remoteAddress} is conected`);
+
   ws.on('error', console.error);
 
   ws.on('message', (data) => {
     const command = data.toString();
-    console.log(command);
 
     if (command.includes('mouse')) {
       if (command.includes('position')) {
         const coord = mouseControler.mouseMove(command.split('_')[1]);
         coord.then((point) => {
           wss.clients.forEach((client) => {
-            client.send(`mouse_position ${point.x},${point.y}`);
+            if (client.readyState === WebSocket.OPEN) {
+              client.send(`mouse_position ${point.x},${point.y}`);
+            }
           });
         }).catch((error) => {
           console.log(error.message);
@@ -30,4 +43,27 @@ wss.on('connection', (ws) => {
   });
 
   ws.send('something');
+
+  ws.on('close', () => {
+    console.log('Client is closed');
+  });
+
+  ws.on('pong', heartbeat);
+});
+
+const interval = setInterval(() => {
+  wss.clients.forEach((ws: MyWS) => {
+    if (ws.isAlive === false) {
+      return ws.terminate();
+    }
+
+    ws.isAlive = false;
+    ws.ping();
+
+    return undefined;
+  });
+}, 30000);
+
+wss.on('close', () => {
+  clearInterval(interval);
 });
